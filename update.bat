ff00f7821ee82ae381cfa74e125600be1eabbb9f
Elias Muresan <eliasmuresan@gmail.com>
2025-12-08 15:59:12 +0200

    test

--- update.bat
+++ update.bat
@@ -1,7 +1,7 @@
 @echo off
-cd /d "%~dp0"
 
-<<<<<<< HEAD
+cd /d "D:\dekstop nou\Comunitea Regionala Penticostala Arad , site 1"
+
 echo =====================================
 echo  Urcare automata site pe GitHub
 echo =====================================
@@ -25,35 +25,5 @@ echo Trimit modificarile pe GitHub (branch main)...
 git push origin main
 
 echo.
-echo Gata. Verifica pe eliasmuresan.github.io dupa ce termine GitHub Pages.
-=======
-echo ==========================
-echo   GIT UPDATE - CRP SITE
-echo ==========================
-echo.
-
-REM 1. Arătăm statusul
-git status
-echo.
-
-REM 2. Adăugăm toate modificările
-git add .
-echo.
-
-REM 3. Facem commit (dacă nu e nimic de commit, continuă mai departe)
-git commit -m "Update site (CSS + slider)" || echo Nici o modificare de commit...
-echo.
-
-REM 4. Tragem ce e nou de pe GitHub (branch main)
-git pull origin main
-echo.
-
-REM 5. Trimitem modificările pe GitHub
-git push origin main
-echo.
-
-echo --------------------------
-echo   GATA - UPDATE TERMINAT
-echo --------------------------
->>>>>>> 1f3fc14ee17a71fae2a0cabd4bd7302ff50d1bbb
+echo Gata. Verifica site-ul dupa ce termina GitHub Pages.
 pause
